142e178c071443c513320201371e23becf48f927
dccmmtop <dccmmtop@gmail.com>
2017-06-17 16:03:31 +0800

    add my table.sql file

--- table.sql
+++ table.sql
@@ -680,3 +680,335 @@ insert into en_ch (en,ch) values('liuxin','刘歆@@');
 insert into en_ch (en,ch) values('cup','n.@@杯子；一杯之量；奖杯；罩杯@@vt.@@使成杯形；把某物放入杯中@@');
  
 insert into en_ch (en,ch) values('dup','n.@@复制品，副本，重复@@');
+ 
+insert into en_ch (en,ch) values('god','n.@@神；偶像；(复数)剧院顶层楼座的观众；(大写)上帝@@int.@@天呐@@');
+ 
+insert into en_ch (en,ch) values('the','art.@@那；这；这些；那些@@adv.@@（用于比较级前）更加；用于最高级前；(用于形容词、副词比较级前)越 ... 越 ...@@');
+ 
+insert into en_ch (en,ch) values('let','v.@@让；允许；(用于祈使句表建议、请求、命令)让；假设；出租@@n.@@出租；阻碍；(网球等)触网重发@@');
+ 
+insert into en_ch (en,ch) values('there','adv.@@在那里；到那里；从那里@@pron.@@表示存在（与be连用）@@n.@@那个地方；那一点@@');
+ 
+insert into en_ch (en,ch) values('was','v.@@是（is，am的过去式）@@');
+ 
+insert into en_ch (en,ch) values('day','n.@@白天；一天；时代@@');
+ 
+insert into en_ch (en,ch) values('call','vt.@@呼叫；打电话；把 ...称为@@n.@@打电话；访问；召唤；呼叫；把 ... 看作@@vi.@@呼叫；(短暂的)拜访@@');
+ 
+insert into en_ch (en,ch) values('call','vt.@@呼叫；打电话；把 ...称为@@n.@@打电话；访问；召唤；呼叫；把 ... 看作@@vi.@@呼叫；(短暂的)拜访@@');
+ 
+insert into en_ch (en,ch) values('morning','n.@@早晨；上午；开端@@');
+ 
+insert into en_ch (en,ch) values('said','adj.@@上述的@@v.@@动词say的过去式和过去分词形式，说，讲@@');
+ 
+insert into en_ch (en,ch) values('i','pron.@@我@@');
+ 
+insert into en_ch (en,ch) values('holy','adj.@@神圣的；圣洁的；令人敬仰的@@');
+ 
+insert into en_ch (en,ch) values('bible','n.@@圣经；（b~）有权威的书@@');
+ 
+insert into en_ch (en,ch) values('in','prep.@@在 ... 里；在 ... 地方；在 ... 期间；在 ... 方面；进入...里面；处于...状态；穿着...；以...的方式@@adv.@@在家；入；进；向里；在某地；并入；在某种关系中@@adj.@@在里面的；新来的；执政的；时髦的@@n.@@当权者；影响；达成目标的路径@@');
+ 
+insert into en_ch (en,ch) values('english','n.@@英语；英国人@@adj.@@英语的；英国人的；英国的@@');
+ 
+insert into en_ch (en,ch) values('textfile','n.@@文本文件@@');
+ 
+insert into en_ch (en,ch) values('first','adj.@@第一的；最初的@@adv.@@首先；第一@@n.@@最初；(序数词)第一@@');
+ 
+insert into en_ch (en,ch) values('made','adj.@@制造的；捏造的；拼成的；肯定会成功的@@动词make的过去式和过去分词形式.@@');
+ 
+insert into en_ch (en,ch) values('heaven','n.@@天空；天堂@@Heaven.@@n.@@上帝@@');
+ 
+insert into en_ch (en,ch) values('earth.','n.@@地球；泥土；世间@@');
+ 
+insert into en_ch (en,ch) values('earth','n.@@地球；泥土；世间@@');
+ 
+insert into en_ch (en,ch) values('waste','n.@@废物；浪费；荒地@@v.@@浪费；滥用；消耗@@adj.@@荒芜的；无用的；浪费的@@');
+ 
+insert into en_ch (en,ch) values('without','prep.@@没有；无；在外面@@adv.@@在缺乏的情况下；在外面@@conj.@@除非@@');
+ 
+insert into en_ch (en,ch) values('it','pron.@@它@@');
+ 
+insert into en_ch (en,ch) values('dark','adj.@@黑暗的；深色的@@n.@@黑暗；暗处；深色调@@v.@@使变黑；变黑@@');
+ 
+insert into en_ch (en,ch) values('on','prep.@@在 ... 之上；由 ... 支撑着；在（某一天）；关于；基于@@adv.@@穿着；向前（移动)；表示持续性@@adj.@@表示已连接、处于工作状态或使用中；发生；值班@@');
+ 
+insert into en_ch (en,ch) values('face','n.@@表面；脸；表情@@v.@@面对；面向；朝@@');
+ 
+insert into en_ch (en,ch) values('of','prep.@@关于；... 的（表所属）；出身于；由于@@');
+ 
+insert into en_ch (en,ch) values('spirit','n.@@精神；心灵；幽灵；精灵@@(复)spirits：精力；情绪；烈酒.@@v.@@使精神振作；偷偷带走@@');
+ 
+insert into en_ch (en,ch) values('moving','adj.@@ 移动的； 使 ... 移动的； 令人感动的@@动词move的现在分词形式@@');
+ 
+insert into en_ch (en,ch) values('waters.','n.@@水域；领海；水体@@Waters.@@n.@@沃特斯(姓氏)@@');
+ 
+insert into en_ch (en,ch) values('be','v.@@有；在；是；到达；拜访@@');
+ 
+insert into en_ch (en,ch) values('light.','n.@@光；光线；灯；打火机；领悟；浅色；天窗@@adj.@@轻的；浅色的；明亮的；轻松的；容易的；清淡的@@v.@@点燃；变亮；照亮@@adv.@@清楚地；轻便地@@');
+ 
+insert into en_ch (en,ch) values('looking','adj.@@有 ... 样子的；有 ... 相貌的@@动词look的现在分词@@');
+ 
+insert into en_ch (en,ch) values('saw','n.@@锯@@v.@@用锯子锯；前后移动；看见，明白，了解（动词see的过去式）@@');
+ 
+insert into en_ch (en,ch) values('that','pron.@@那@@conj.@@引导从句@@adj.@@那个；上文提到的@@adv.@@非常；那么地@@');
+ 
+insert into en_ch (en,ch) values('division','n.@@划分；除法；部门；分开；师（军队）@@');
+ 
+insert into en_ch (en,ch) values('light','n.@@光；光线；灯；打火机；领悟；浅色；天窗@@adj.@@轻的；浅色的；明亮的；轻松的；容易的；清淡的@@v.@@点燃；变亮；照亮@@adv.@@清楚地；轻便地@@');
+ 
+insert into en_ch (en,ch) values('naming','n.@@命名@@动词name的现在分词形式.@@');
+ 
+insert into en_ch (en,ch) values('night.','n.@@夜晚@@adj.@@夜晚的@@');
+ 
+insert into en_ch (en,ch) values('evening','n.@@晚上；傍晚；晚会@@');
+ 
+insert into en_ch (en,ch) values('day.','n.@@白天；一天；时代@@');
+ 
+insert into en_ch (en,ch) values('solid','adj.@@固体的；实心的；结实的；可靠的@@n.@@固体；立体图形@@adv.@@一致地@@');
+ 
+insert into en_ch (en,ch) values('arch','n.@@拱；拱门；拱状物@@v.@@成拱形；拱起@@adj.@@主要的；调皮的@@');
+ 
+insert into en_ch (en,ch) values('stretching','n.@@拉伸@@动词stretch的现在分词形式.@@');
+ 
+insert into en_ch (en,ch) values('over','prep.@@在 ... 的上方；遍及地；在 ... 期间@@adv.@@穿过；完全覆盖；结束；落下@@n.@@(板球中)每个投球手一次连续所投出的球数@@');
+ 
+insert into en_ch (en,ch) values('parting','n.@@分别；头发分缝；分歧@@adj.@@离别的；逝去的@@动词part的现在分词形式.@@');
+ 
+insert into en_ch (en,ch) values('waters','n.@@水域；领海；水体@@Waters.@@n.@@沃特斯(姓氏)@@');
+ 
+insert into en_ch (en,ch) values('from','prep.@@出自；来自；从( ... 起)@@');
+ 
+insert into en_ch (en,ch) values('for','prep.@@为；因为；代表；往；支持；关于；给@@conj.@@因为@@');
+ 
+insert into en_ch (en,ch) values('which','adj.@@哪一个；哪一些@@pron.@@哪一个；哪些@@');
+ 
+insert into en_ch (en,ch) values('were','v.@@是，在，有（be动词的复数过去式）@@');
+ 
+insert into en_ch (en,ch) values('under','prep.@@低于；在 ... 下；在 ... 内；在 ... 控制下@@adv.@@在昏迷中；在 ... 下@@adj.@@下面的；从属的；少于的@@');
+ 
+insert into en_ch (en,ch) values('those','pron.@@那些@@adj.@@那些@@');
+ 
+insert into en_ch (en,ch) values('so.','adv.@@那么；这样；如此@@adj.@@真的；非常@@pron.@@这样@@conj.@@因此；所以；以便@@');
+ 
+insert into en_ch (en,ch) values('gave','v.@@给；赠予；送（give的过去式）@@');
+ 
+insert into en_ch (en,ch) values('name','n.@@名称；姓名；名义；名誉；名人@@v.@@命名；取名；说出( ... 的名字)@@adj.@@著名的@@');
+ 
+insert into en_ch (en,ch) values('heaven.','n.@@天空；天堂@@Heaven.@@n.@@上帝@@');
+ 
+insert into en_ch (en,ch) values('in','prep.@@在 ... 里；在 ... 地方；在 ... 期间；在 ... 方面；进入...里面；处于...状态；穿着...；以...的方式@@adv.@@在家；入；进；向里；在某地；并入；在某种关系中@@adj.@@在里面的；新来的；执政的；时髦的@@n.@@当权者；影响；达成目标的路径@@');
+ 
+insert into en_ch (en,ch) values('night','n.@@夜晚@@adj.@@夜晚的@@');
+ 
+insert into en_ch (en,ch) values('cain','n.@@该隐；杀弟者；凶手@@');
+ 
+insert into en_ch (en,ch) values('went','动词go的过去式形式@@');
+ 
+insert into en_ch (en,ch) values('his','adj.@@他的@@pron.@@他的@@');
+ 
+insert into en_ch (en,ch) values('in','prep.@@在 ... 里；在 ... 地方；在 ... 期间；在 ... 方面；进入...里面；处于...状态；穿着...；以...的方式@@adv.@@在家；入；进；向里；在某地；并入；在某种关系中@@adj.@@在里面的；新来的；执政的；时髦的@@n.@@当权者；影响；达成目标的路径@@');
+ 
+insert into en_ch (en,ch) values('land','n.@@陆地；国土；土地@@v.@@靠岸；着陆；跌落地面；得到；使靠岸；使着陆@@');
+ 
+insert into en_ch (en,ch) values('nod','v.@@点头；打盹；（使）摆动@@n.@@点头；同意@@');
+ 
+insert into en_ch (en,ch) values('east','adj.@@东方的@@adv.@@向东方@@n.@@东方；东部@@');
+ 
+insert into en_ch (en,ch) values('eden','n.@@伊甸园；乐园@@');
+ 
+insert into en_ch (en,ch) values('had','v.@@有；吃；得到（动词have的过去式和过去分词）@@aux.@@已经（用于过去完成时和过去完成进行时）@@');
+ 
+insert into en_ch (en,ch) values('connection','n.@@联系；关系；连接；亲戚@@');
+ 
+insert into en_ch (en,ch) values('with','prep.@@用；随着；包括；和 ... 一起@@');
+ 
+insert into en_ch (en,ch) values('wife','n.@@妻子；夫人@@');
+ 
+insert into en_ch (en,ch) values('she','pron.@@她（主格）@@');
+ 
+insert into en_ch (en,ch) values('became','v.@@变成；成为；变得（动词become的过去式）@@');
+ 
+insert into en_ch (en,ch) values('child','n.@@小孩；子女；产物@@');
+ 
+insert into en_ch (en,ch) values('to','prep.@@对于；为了；(表示方向)到；向；(表示间接关系)给@@adv.@@（表示方向）去；（门）关上@@');
+ 
+insert into en_ch (en,ch) values('he','pron.@@他；任何人@@n.@@男人；雄性动物@@');
+ 
+insert into en_ch (en,ch) values('town','n.@@城市；城镇；（某一城镇的）居民；商业区@@');
+ 
+insert into en_ch (en,ch) values('enoch','n.@@伊诺克(男子名)@@');
+ 
+insert into en_ch (en,ch) values('son','n.@@儿子；孩子（长者对后辈的称呼）@@');
+ 
+insert into en_ch (en,ch) values('father','n.@@父亲；神父；祖先；创始人@@v.@@创造；当 ... 的父亲@@');
+ 
+insert into en_ch (en,ch) values('two','num.@@二；两个@@');
+ 
+insert into en_ch (en,ch) values('other','adj.@@其他的；另外的；另一个；另一边@@pron.@@其他(人或物)@@n.@@其他人(或事)@@adv.@@以其他方式@@');
+ 
+insert into en_ch (en,ch) values('zillah','n.@@郡@@');
+ 
+insert into en_ch (en,ch) values('adah','n.@@艾达(f@@');
+ 
+insert into en_ch (en,ch) values('such','adj.@@这样的；如此的@@adv.@@如此地@@pron.@@这样的人或事；其本身；这；那@@');
+ 
+insert into en_ch (en,ch) values('are','v.@@是、在(系动词be的现在时、用于第二人称或复数)@@');
+ 
+insert into en_ch (en,ch) values('living','adj.@@活着的；生动的@@n.@@生活；生计@@动词live的现在分词.@@');
+ 
+insert into en_ch (en,ch) values('tents','n.@@旅游帐篷@@名词tent的复数形式.@@');
+ 
+insert into en_ch (en,ch) values('keep','v.@@保持；保留；继续；贮藏；经营；遵守；供给；照管；饲养；耽搁；保持食物不变质；健康状况如何；保守（秘密@）；记录；遵守；保护@@n.@@生计；保存；城堡主楼；监狱；围墙@@');
+ 
+insert into en_ch (en,ch) values('cattle','n.@@牛；家畜；畜牲@@');
+ 
+insert into en_ch (en,ch) values('jabal','雅巴尔@@');
+ 
+insert into en_ch (en,ch) values('jubal','n.@@犹八(基督教《圣经》故事人物；创制乐器的始祖)@@');
+ 
+insert into en_ch (en,ch) values('ways','n.@@(用作单数)途径；方法@@名词way的复数形式.@@');
+ 
+insert into en_ch (en,ch) values('three','num.@@三@@n.@@三；三个@@adj.@@三个的@@');
+ 
+insert into en_ch (en,ch) values('hundred','num.@@百@@n.@@一百元@@（复）hundreds: 成百上千；许多.@@');
+ 
+insert into en_ch (en,ch) values('sons','n.@@子弟；儿子@@名词son的复数形式.@@');
+ 
+insert into en_ch (en,ch) values('years','n.@@很久；年，年度@@名词year的复数形式.@@');
+ 
+insert into en_ch (en,ch) values('life','n.@@一生；生命；生活；人生@@');
+ 
+insert into en_ch (en,ch) values('not','adv.@@不；没有；并非@@');
+ 
+insert into en_ch (en,ch) values('seen','v.@@看见，理解（动词see的过去分词）@@adj.@@看得见的@@');
+ 
+insert into en_ch (en,ch) values('took','v.@@拿；取；执行；需要；接受；理解；修(课程)；花费；吃（喝）（动词take的过去式）@@');
+ 
+insert into en_ch (en,ch) values('him','pron.@@他(he的宾格)@@');
+ 
+insert into en_ch (en,ch) values('methuselah','n.@@玛士撒拉；非常高寿的人@@');
+ 
+insert into en_ch (en,ch) values('eighty-seven','num.@@八十七@@');
+ 
+insert into en_ch (en,ch) values('old','adj.@@老的；过去的；旧的，破旧的；... 岁的@@n.@@旧时@@');
+ 
+insert into en_ch (en,ch) values('seven','num.@@七@@');
+ 
+insert into en_ch (en,ch) values('eighty-two','num.@@八十二@@');
+ 
+insert into en_ch (en,ch) values('nine','num.@@九@@');
+ 
+insert into en_ch (en,ch) values('came','v.@@来；来到；来取、来拿；成为；达到；接近；摆出 ... 的样子；处于（某个位置）@@');
+ 
+insert into en_ch (en,ch) values('end','n.@@结束；末端；目标；死亡；部分；终点站；端点；半边球场；剩余物，残余；过世@@v.@@终止，结束@@');
+ 
+insert into en_ch (en,ch) values('will','n.@@决心；意志；遗嘱；愿望@@aux.@@将；愿意；必须@@vt.@@决心；想要(某事发生)；将(财产)遗赠；规定@@vi.@@愿意@@');
+ 
+insert into en_ch (en,ch) values('give','v.@@给；赠予；送；捐助；提供；供给；举行；弯曲@@n.@@弹性@@');
+ 
+insert into en_ch (en,ch) values('us','pron.@@我们(we的宾格)@@');
+ 
+insert into en_ch (en,ch) values('rest','n.@@剩余的部分；休息；支撑物；休止符@@v.@@休息；(使)倚靠；使(视线)停留在；搁在；依赖；基于；搁置；埋葬@@');
+ 
+insert into en_ch (en,ch) values('our','adj.@@我们的@@');
+ 
+insert into en_ch (en,ch) values('trouble','n.@@困难；烦恼；麻烦@@v.@@使烦恼；麻烦；苦恼；费神@@');
+ 
+insert into en_ch (en,ch) values('hard','adj.@@硬的；困难的；棘手的；坚强的；残酷的；用力的；液态的；严寒的；确凿的@@adv.@@努力地；强烈地；严重地；坚定地@@');
+ 
+insert into en_ch (en,ch) values('cursed','adj.@@被诅咒的；可恶的；讨厌的@@动词curse的过去式和过去分词.@@');
+ 
+insert into en_ch (en,ch) values('five','num.@@五@@');
+ 
+insert into en_ch (en,ch) values('ninety-five','num.@@九十五@@');
+ 
+insert into en_ch (en,ch) values('noah','n.@@[圣]诺亚(男子名)@@');
+ 
+insert into en_ch (en,ch) values('japheth','n.@@[圣经] 雅弗(诺亚的第三个儿子)@@');
+ 
+insert into en_ch (en,ch) values('men','n.@@男人；人@@名词man的复数形式.@@');
+ 
+insert into en_ch (en,ch) values('increasing','adj.@@增长的；增加的@@');
+ 
+insert into en_ch (en,ch) values('daughters','n.@@多特斯@@');
+ 
+insert into en_ch (en,ch) values('they','pron.@@他们@@');
+ 
+insert into en_ch (en,ch) values('wives','n.@@妻子@@名词wife的复数形式.@@');
+ 
+insert into en_ch (en,ch) values('themselves','pron.@@他/她/它们自己；他/她/它们亲自；指性别不详的人时用以代替himself或herself@@himself/herself/itself的复数.@@');
+ 
+insert into en_ch (en,ch) values('who','pron.@@谁@@abbr.@@世界卫生组织(=World Health Organization)@@');
+ 
+insert into en_ch (en,ch) values('pleasing','adj.@@使人满意的；令人愉快的；讨人喜爱的@@动词please的现在分词.@@');
+ 
+insert into en_ch (en,ch) values('them','pron.@@他们；她们；它们；性别不详的人（代替him或her）@@');
+ 
+insert into en_ch (en,ch) values('shell','n.@@壳；炮弹；[计]外壳程序@@v.@@脱落；去壳；拾贝壳；炮轰@@');
+ 
+insert into en_ch (en,ch) values('or','conj.@@或者；还是；否则@@prep.@@在...之前@@');
+ 
+insert into en_ch (en,ch) values('majority','n.@@大多数；多数；多数党；多数派@@');
+ 
+insert into en_ch (en,ch) values('cx','[计] 上下文环境命令@@');
+ 
+insert into en_ch (en,ch) values('lake','n.@@湖；湖泊@@n.@@深红色；[化]色淀@@');
+ 
+insert into en_ch (en,ch) values('new','adj.@@新的；崭新的；新鲜的；不熟悉的@@adv.@@新近地@@');
+ 
+insert into en_ch (en,ch) values('zealand','n.@@西兰岛(丹麦最大的岛)@@');
+ 
+insert into en_ch (en,ch) values('links','n.@@高尔夫球场；沙丘；环，纽带（名词link的复数形式）@@v.@@联系；连接（动词link的第三人称单数形式）@@');
+ 
+insert into en_ch (en,ch) values('mechanize','vt.@@使机械化；用机械操作@@');
+ 
+insert into en_ch (en,ch) values('ruby','adj.@@红宝石色的@@n.@@红宝石；红宝石色@@Ruby：露比（人名）.@@');
+ 
+insert into en_ch (en,ch) values('courage','n.@@勇气；胆量@@');
+ 
+insert into en_ch (en,ch) values('open','adj.@@开着的；开放的；公开的@@v.@@打开；张开；展开；摊开；开始(某事物)；开张；营业；以…开头；宣布启用@@n.@@户外；公开@@');
+ 
+insert into en_ch (en,ch) values('matter','n.@@事情；事态；问题；关于...的事情；物质；重要性@@v.@@有关系；要紧@@');
+ 
+insert into en_ch (en,ch) values('take','v.@@拿；取；执行；需要；接受；理解；修(课程)；花费；吃（喝）；认为；搭乘@@n.@@拿取；取得物@@');
+ 
+insert into en_ch (en,ch) values('only','adv.@@只有；仅仅；刚刚@@adj.@@最好的；唯一的；出众的@@conj.@@但是；除非；然而；毕竟@@');
+ 
+insert into en_ch (en,ch) values('know','v.@@了解；知道；认识@@n.@@知情@@@@');
+ 
+insert into en_ch (en,ch) values('run','v.@@跑；行驶；运转；运营；持续；蔓延；传播；竞选；遭遇；熔化；褪色；流@@n.@@路程；奔跑；趋向@@adj.@@熔化的；浇铸的；洄游的@@');
+ 
+insert into en_ch (en,ch) values('baidu','n.@@百度（搜索引擎名）@@');
+ 
+insert into en_ch (en,ch) values('forever','adv.@@永远@@');
+ 
+insert into en_ch (en,ch) values('woman','n.@@妇女；女人@@adj.@@女人的@@');
+ 
+insert into en_ch (en,ch) values('good','adj.@@好的；上等的；优秀的@@n.@@好处；善行@@(复)goods：商品；货物.@@');
+ 
+insert into en_ch (en,ch) values('is','v.@@是（be的第三人称单数一般现在时）@@');
+ 
+insert into en_ch (en,ch) values('web','n.@@网；网络；网状物；腹板@@vi.@@结网@@vt.@@结网于；使陷入罗网@@');
+ 
+insert into en_ch (en,ch) values('malware','n.@@恶意软件@@');
+ 
+insert into en_ch (en,ch) values('shareware','n.@@共享软件@@');
+ 
+insert into en_ch (en,ch) values('kerberos','n.@@希腊神话中把守冥府入口的三头狗@@n.@@一种网络密码验证授权系统@@');
+ 
+insert into en_ch (en,ch) values('java','n.@@爪哇岛(印度尼西亚一岛)@@');
+ 
+insert into en_ch (en,ch) values('blogs','n.@@博客；部落格；网络日志（名词blog的复数形式）@@');
+ 
+insert into en_ch (en,ch) values('validate','vt.@@使生效；证实；确认；验证@@');
+ 
+insert into en_ch (en,ch) values('remote','adj.@@遥远的；偏僻的；远程的；(感情等)距离很大@@n.@@远程操作@@');
+ 
+insert into en_ch (en,ch) values('object','n.@@物体；目标；对象；宾语@@v.@@反对；提出 ... 作为反对的理由@@');
+ 
+insert into en_ch (en,ch) values('engine','n.@@发动机；引擎；机车；火车头@@vt.@@给 ... 装引擎@@');
+ 
+insert into en_ch (en,ch) values('signal','n.@@信号；标志@@v.@@(发信号)通知、表示@@adj.@@显著的@@');
+ 
+insert into en_ch (en,ch) values('wait','v.@@等；等待@@n.@@等待；等候@@');
